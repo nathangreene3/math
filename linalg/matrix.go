cbea48b7eaee5c4698ddfe31a434c1bc79f337fa
Nathan Greene <nathangreene3@gmail.com>
2020-09-18 00:56:04 -0400

    Interfaces?

--- linalg/matrix.go
+++ linalg/matrix.go
@@ -2,9 +2,17 @@ package linalg
 
 // Matrix ...
 type Matrix interface {
-	Get(i, j int) float64
-	Set(i, j int, v float64)
+	MatrixReader
+	MatrixWriter
 }
+
+// MatrixReader ...
+type MatrixReader interface {
+	Get(i, j int) float64
+}
+
+// MatrixWriter ...
+type MatrixWriter interface{ Set(i, j int) float64 }
 
 // LongMatrix ...
 type LongMatrix struct {
@@ -30,4 +38,27 @@ func New(m, n int, f Generator) *LongMatrix {
 	}
 
 	return &A
+}
+
+// Add ...
+func (A *LongMatrix) Add(B *LongMatrix) {
+	switch {
+	case len(A.matrix) != len(B.matrix):
+	case A.m != B.m:
+	case A.n != B.n:
+	}
+
+	for i := 0; i < A.m*A.n; i++ {
+		A.matrix[i] += B.matrix[i]
+	}
+}
+
+// Get ...
+func (A *LongMatrix) Get(i, j int) float64 {
+	return A.matrix[i*A.m+j]
+}
+
+// Set ...
+func (A *LongMatrix) Set(i, j int, v float64) {
+	A.matrix[i*A.m+j] = v
 }
